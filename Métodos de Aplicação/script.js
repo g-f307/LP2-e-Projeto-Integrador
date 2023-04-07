9feebc224dd6e8f30fa75c7bee8d11ff7c91a953
g-f307 <119821327+g-f307@users.noreply.github.com>
2023-04-07 19:35:10 -0300

    Atualizar o script.js

--- Métodos de Aplicação/script.js
+++ Métodos de Aplicação/script.js
@@ -1,15 +1,12 @@
-// Busca os dados do JSON
+
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        // Cria o HTML para cada vídeo-aula
         const videosHTML = data.videos.map(video => `
             <div class="p-container2">
                 <h3>${video.title}</h3>
                 <iframe width="560" height="315" src="${video.url}" frameborder="0" allowfullscreen></iframe>
             </div>
         `).join('');
-
-        // Insere o HTML na página
         document.getElementById('videos').innerHTML = videosHTML;
     });
