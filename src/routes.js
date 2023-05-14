ab17db0f8687d4e9a00fe73c8abb7cbdb115c5e0
g-f307 <gabrielsa720@gmail.com>
2023-05-14 17:28:28 -0300

    app.js otimizado

--- src/routes.js
+++ src/routes.js
@@ -2,31 +2,33 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-// Rota para listar todas as vídeo-aulas
 router.get('/', (req, res) => {
-  // Lendo o arquivo JSON com as informações das aulas
   const aulas = JSON.parse(fs.readFileSync('aulas.json'));
-
-  // Enviando a lista de aulas como resposta da requisição
   res.send(aulas);
 });
 
-// Rota para listar uma vídeo-aula específica pelo seu ID
 router.get('/:id', (req, res) => {
-  // Lendo o arquivo JSON com as informações das aulas
   const aulas = JSON.parse(fs.readFileSync('aulas.json'));
-
-  // Buscando a aula com o ID especificado na URL
   const aula = aulas.find(aula => aula.id == req.params.id);
-
-  // Verificando se a aula foi encontrada
   if (aula) {
-    // Enviando a aula encontrada como resposta da requisição
     res.send(aula);
   } else {
-    // Enviando um erro 404 caso a aula não seja encontrada
     res.status(404).send('Aula não encontrada');
   }
+});
+
+router.post('aulas.json', (req, res) => {
+  const database = JSON.parse(fs.readFileSync('aulas.json'));
+  const newVideo = {
+    id: database.videos.length + 1,
+    title: req.body.title,
+    description: req.body.description,
+    url: req.body.url,
+    duration: req.body.duration
+  };
+  database.videos.push(newVideo);
+  fs.writeFileSync('aulas.json', JSON.stringify(database));
+  res.json(newVideo);
 });
 
 module.exports = router;
